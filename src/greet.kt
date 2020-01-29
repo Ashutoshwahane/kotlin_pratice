978546839144029794528b5511206d820d92c597
willskhalifa <ashutosh.wahane10@gmail.com>
2020-01-30 00:02:18 +0530

    getter and setter in kotlin

--- src/greet.kt
+++ src/greet.kt
@@ -1,9 +1,14 @@
 fun main(args: Array<String>){
     println("Welcome to kotlin world")
 
-    var obj = User()
+//    var obj = User()
+//
+//    obj.name = "Ashutosh"
+//
+//    println("Name is :${obj.name}")
 
-    obj.name = "Ashutosh"
-
-    println("Name is :${obj.name}")
+    //setting value without calling set() method
+    var obj_learn = learn()
+    obj_learn.data = "Kotline"
+    println("learn : ${obj_learn.data}")
 }
